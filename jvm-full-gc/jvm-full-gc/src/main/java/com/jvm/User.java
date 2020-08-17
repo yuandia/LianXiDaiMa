797e4a13aa2da97fec49ca18b85491bbade3843e
YuanDian <1378516231@qq.com>
2020-08-17 21:13:16 +0800

    first commit

---
+++ jvm-full-gc/jvm-full-gc/src/main/java/com/jvm/User.java
@@ -0,0 +1,40 @@
+package com.jvm;
+
+import java.util.Arrays;
+
+public class User {
+	
+	private int id;
+	private String name;
+
+	byte[] a = new byte[1024*100];
+
+	public User(){}
+
+	public User(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"id=" + id +
+				", name='" + name + '\'' +
+				'}';
+	}
+}
