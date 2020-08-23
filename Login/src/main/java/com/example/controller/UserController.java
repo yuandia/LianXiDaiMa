01f0b2f9e2f3d375000515af73cfe9fd2cb652a1
YuanDian <1378516231@qq.com>
2020-08-23 16:38:03 +0800

    first commit

---
+++ Login/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.controller;
+
+import com.example.bean.Result;
+import com.example.bean.User;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController    //相当于@Controller+@RequestBody
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    /**
+     * 注册
+     * @param user 参数封装
+     * @return Result
+     */
+    @PostMapping(value = "/regist")
+    public Result regist(User user){
+        return userService.regist(user);
+    }
+    /**
+     * 登录
+     * @param user 参数封装
+     * @return Result
+     */
+    @PostMapping(value = "/login")
+    public Result login(User user){
+        return userService.login(user);
+    }
+}
